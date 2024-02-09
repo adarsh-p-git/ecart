8da679663fbd971b9c333439e558a9ac4ab0e159
adarsh-p-git <adarshp2020@gmail.com>
2024-02-09 14:28:06 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import logo from './logo.svg';
+
+import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Cart from './pages/Cart';
+import Home from './pages/Home';
+import Wishlist from './pages/Wishlist';
+import {Route,Routes} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      E-Cart
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/wishlist' element={<Wishlist/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+      </Routes>
+      <Footer/>
     </div>
   );
 }
